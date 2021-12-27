0002a8b626e92fbcd1613c20d0e26f6acfbb78bf
MinyeongHonggit config --global user.email agka00161@gmail.comcd C:/Users/user/Desktop/Git <78523467+MinyeongHong@users.noreply.github.com>
2021-12-27 16:07:10 +0900

    Update 1920.cpp

--- 1920.cpp
+++ 1920.cpp
@@ -1,53 +1,50 @@
-#define _CRT_SECURE_NO_WARNINGS
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-int N, M;
-int n[100001], m[100001] = { 0 };
+vector<int> first_card;
+vector<int> answer;
 
-void binary(int start,int end, int mid,int pivot) {
-	int len = end - start + 1;
-	int mid_left = mid / 2;
-	int mid_right = (end + mid + 1) / 2;
+void ck(int start_idx, int end_idx, int val) {
+    int mid_idx = (end_idx + start_idx) / 2;
+    if (start_idx <= end_idx) {
+        if (first_card[mid_idx] == val) {
+            answer.push_back(1);
+            return;
+        }
+        else if (first_card[mid_idx] < val) {
+            ck(mid_idx + 1, end_idx, val);
+        }
+        else {
+            ck(start_idx, mid_idx - 1, val);
+        }
+    }
+    else {
+        answer.push_back(0);
+        return;
+    }
 
-	if (start > end) {
-		printf("0\n");
-		return;
-	}
-
-	else {
-		if (n[mid] > pivot) {
-			binary(start, mid - 1,mid_left, pivot);
-		}
-
-		else if (n[mid] < pivot) {
-			binary(mid + 1, end, mid_right, pivot);
-		}
-
-		else {
-			printf("1\n");
-			return;
-		}
-	}
-	
+    return;
 }
 
 int main(void) {
+    int N, M, a, b;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> a;
+        first_card.push_back(a);
+    }
+    sort(first_card.begin(), first_card.end());
 
-	scanf("%d", &N);
-	for (int i = 1; i < N+1; i++) scanf("%d", &n[i]);
+    cin >> M;
+    for (int i = 0; i < M; i++) {
+        cin >> b;
+        ck(0, first_card.size() - 1, b);
+    }
 
-	scanf("%d", &M);
-	for (int i = 1; i < M+1; i++) scanf("%d", &m[i]);
-
-	sort(n, n + N + 1);
-
-	for (int i = 1; i < M+1; i++) {
-		binary(1, N, (N+1)/2, m[i]);
-	}
-	
-	return 0;
+    for (auto it = answer.begin(); it < answer.end(); it++) printf("%d\n",*it);
+    return 0;
 }
